ee9b42bbcbea3a81bc24dfc6aeee26c723a8237a
Tatyana Ryzhenkowa <ryzhenkowa.tatyana@yandex.ru>
2022-08-04 00:26:48 +0300

    first

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+// Импортируем пакет path
+
+module.exports = {
+    entry: path.resolve(__dirname, './src/index.js'),
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: [ ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx'],
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js',
+    },
+    devServer: {
+        static: path.join(__dirname, './dist'),
+        compress: true,
+        port: 3000,
+    },
+};
